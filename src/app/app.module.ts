bafe18acf5b794931ed8c18ca3f303ea1135914a
irensvm <irens.vm@gmail.com>
2022-02-24 11:33:36 +0100

    cardStyles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { SearchComponent } from './shared/components/search/search.component';
 import { ItemListComponent } from './components/pages/item-list/item-list.component';
-import { ItemComponent } from './item/item.component';
+import { ItemComponent } from './components/pages/item-list/item/item.component';
 
 @NgModule({
   declarations: [
